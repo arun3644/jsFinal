07a3043786ebb80cdbd2550f2242335fba5935d0
ARUNKUMAR <145777038+arun3644@users.noreply.github.com>
2025-05-20 18:01:33 +0530

    Add files via upload

---
+++ front-end/Js/Es(6)/module/main.js
@@ -0,0 +1,4 @@
+// import * as math from './math.js'; 
+import {add, sub, mul, div} from './math.js';
+
+console.log(add(3, 4));
--- front-end/Js/json/json.js
+++ front-end/Js/json/json.js
@@ -1,56 +1,152 @@
+function get(){
+    const http = new XMLHttpRequest();
+    http.onload=function(){
+        const result=document.getElementById("result");
+
+        const students=JSON.parse(this.responseText);
+        let output= `
+            <table border="1 " cellpadding="5" cellspacing="0" >
+                <tr>
+                    <th>NAME</th>
+                    <th>ROLL NO</th>
+                    <th>EMAIL</th>
+                    <th>COLLEGE NAME</th>
+                </tr>
+        `;
+
+        students.forEach((s)=>{
+            output+=`
+                <tr>
+                    <td>${s.name}</td>
+                    <td>${s.rollno}</td>
+                    <td>${s.email}</td>
+                    <td>${s.college}</td>
+                </tr>
+            `;
+        })
+
+        output +="</table>";
+
+        result.innerHTML=output;
+
+    }
+    http.open("GET","http://localhost:3000/student");
+    http.send();
+}
+
 function add(){
-    const http=new XMLHttpRequest();
+        const http = new XMLHttpRequest();
 
     const data={
-        id:  Date.now(),
-        name:document.getElementById('name').value,
-        rollNo:document.getElementById("rollNo").value,
+        id: Date.now(),
+        name:document.getElementById("name").value,
         email:document.getElementById("email").value,
+        rollno:parseInt(document.getElementById("rollno").value),
         college:document.getElementById("college").value
-
     }
     http.onload=function(){
-        alert("data inserted Successfully");
+        alert("Data Added Successfully");
     }
-    http.open("POST",'http://localhost:3001/student');
-    http.setRequestHeader("Content-Type", "application/json");
+    http.open("POST","http://localhost:3000/student");
     http.send(JSON.stringify(data));
 }
 
 
-function get(){
-    const http=new XMLHttpRequest();
-    http.onload=function(){
-        const result=document.getElementById('result');
 
-        const students= JSON.parse(this.responseText);
-        let output = `
-        <table border="1" cellpadding="5" cellspacing="0">
-            <tr>
-                <th>NAME</th>
-                <th>ROLL NO</th>
-                <th>EMAIL</th>
-                <th>COLLeGE</th>
-            </tr>      
-        `;
 
-        students.forEach((s)=>{
-            output += `
-            <tr>
-                <td>${s.name}</td>
-                <td>${s.rollNo}</td>
-                <td>${s.email}</td>
-                <td>${s.college}</td> 
-            </tr>  
-        `;
-        })
 
-        output += "</table>"
 
-       result.innerHTML=output;
-        
-    }
-    http.open("GET","http://localhost:3001/student");
-    http.send();
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function get() {
+//     fetch("http://localhost:3000/student")
+//         .then((response) => {
+//             if (!response.ok) throw new Error("Failed to Fetch Data")
+//             return response.json();
+//         })
+//         .then(students => {
+//             const result = document.getElementById("result");
+//             let output = `
+//             <table border="1 " cellpadding="5" cellspacing="0" >
+//                 <tr>
+//                     <th>NAME</th>
+//                     <th>ROLL NO</th>
+//                     <th>EMAIL</th>
+//                     <th>COLLEGE NAME</th>
+//                 </tr>
+//         `;
+
+//             students.forEach((s) => {
+//                 output += `
+//                 <tr>
+//                     <td>${s.name}</td>
+//                     <td>${s.rollno}</td>
+//                     <td>${s.email}</td>
+//                     <td>${s.college}</td>
+//                 </tr>
+//             `;
+//             })
+
+//             output += "</table>";
+
+//             result.innerHTML = output;
+
+//         }).catch(error => {
+//             console.error("Error fetching data:", error);
+//             document.getElementById('result').innerHTML = "Error loading data";
+//         });
+// }
+
+
+// function add() {
+//     const data = {
+//         id: Date.now(),
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         rollno: parseInt(document.getElementById("rollno").value),
+//         college: document.getElementById("college").value
+//     }
+
+//     fetch("http://localhost:3000/student", {
+//         method: "POST",
+//         headers: {
+//             "content-type": "application/json"
+//         },
+
+//         body: JSON.stringify(data)
+//     }
+//     ).then((response)=>{
+//         if(!response.ok) throw new Error("Failed to add Data");
+//         alert("Data Added Successfully")
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//         alert("Failed to add Data");
+//     })
+// }
